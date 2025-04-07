a1f31ef4a42eb2daa5815e77a501f99ddc2d4e79
Rafael F Pessoa <112719981+RafaFonPessoa@users.noreply.github.com>
2025-04-06 22:42:43 -0300

    pagina main criada e rota adicionada

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import {HashRouter as Router, Routes, Route} from 'react-router-dom'
 import {Login} from './pages/login'
 import {SignUp} from './pages/sign-up'
+import { MainPage } from './pages/main-page';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login/>}></Route>
         <Route path="/cadastro" element={<SignUp/>}></Route>
+        <Route path="/ecoluz" element={<MainPage/>}></Route>
       </Routes>
     </Router>
   );
